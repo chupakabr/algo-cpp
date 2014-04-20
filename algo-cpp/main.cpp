42b2d653e2afda6eb9775a5b487f4dc1ebccdfb8
Valeriy Chevtaev <myltik@gmail.com>
2014-04-20 01:01:56 +0100

    initial commit

---
+++ algo-cpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  algo-cpp
+//
+//  Created by Valeriy Chevtaev on 20/04/2014.
+//  Copyright (c) 2014 Valeriy Chevtaev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
